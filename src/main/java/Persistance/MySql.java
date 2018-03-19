f5842e9f493103b3bcdb0ac329c71aeb7d0a8df9
damien sauvalle <damiensauvalle@gmail.com>
2018-03-19 14:13:42 +0100

    Feat : add connection to Mysql

---
+++ src/main/java/Persistance/MySql.java
@@ -0,0 +1,33 @@
+package Persistance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySql {
+
+    private static Connection connection;
+
+    private MySql() {
+        try {
+            initConnexion();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // connection a la base MySql
+    private void initConnexion() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://webtp.fil.univ-lille1.fr/sauvalle", "sauvalle", "toto62");
+    }
+
+    public static Connection getInstance() {
+        if (connection == null) {
+            new MySql();
+        }
+        return connection;
+    }
+}
