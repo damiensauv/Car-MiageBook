4418cb278f8637ab05fdb633703a0c0b899e2a61
damien sauvalle <damiensauvalle@gmail.com>
2018-03-20 22:11:04 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,21 +1,24 @@
 package Service;
 
-import javax.ws.rs.GET;
+import Domain.User;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("api/user")
+@Path("user")
 public class UserService {
 
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("/test")
-    public String getIt() {
-
-
-        return "Got it!";
+    @POST
+    @Path("/signup")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User signUp() {
+        User u = new User();
+        u.setNom("damien");
+        return u;
     }
 
 }
