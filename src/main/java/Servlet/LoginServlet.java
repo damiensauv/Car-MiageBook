a923451af99f1d42fc6dc39937136c4bd6677b3c
damien sauvalle <damiensauvalle@gmail.com>
2018-03-27 11:03:33 +0200

    feat : add basic login page by default

--- src/main/java/Servlet/LoginServlet.java
+++ src/main/java/Servlet/LoginServlet.java
@@ -1,4 +1,20 @@
 package Servlet;
 
-public class LoginServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        RequestDispatcher view = request.getRequestDispatcher("/Template/login.html");
+
+        view.forward(request, response);
+    }
 }
