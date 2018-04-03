9dfcf00711b1dc260a1eddf734465d5bcd4ce22f
damien sauvalle <damiensauvalle@gmail.com>
2018-04-03 10:58:42 +0200

    feat : basic user for persistance

---
+++ src/main/java/Persistance/DataMapper/DataMapper.java
@@ -0,0 +1,47 @@
+package Persistance.DataMapper;
+
+import Persistance.MySql.MySql;
+import Util.IDMap;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class DataMapper<T> {
+
+    // DataMapper Principal qui est extends
+
+    protected Connection connection;
+    protected IDMap<T> idMap;
+
+    public DataMapper() {
+        try {
+            connection = MySql.getInstance();
+            idMap = new IDMap<T>();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    abstract T find(Object id);
+
+    abstract Object insert(T o) throws SQLException;
+
+    abstract void delete(T o);
+
+    abstract void update(T o) throws SQLException;
+
+    public Integer getLastIndexInsert(PreparedStatement ps) {
+        try {
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/Persistance/DataMapper/UserMapper.java
@@ -0,0 +1,103 @@
+package Persistance.DataMapper;
+
+import Domain.Interface.IUser;
+import Domain.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper extends DataMapper<IUser> {
+
+    private static UserMapper instance = null;
+
+    public static UserMapper getInstance() {
+        if (instance == null) {
+            instance = new UserMapper();
+        }
+        return instance;
+    }
+
+    private UserMapper() {
+    }
+
+    private User createUser(ResultSet rs) throws SQLException {
+
+/*        User p = new User();
+        p.setId(rs.getInt(1));
+        p.setUsername(rs.getString(2));
+        p.setPassword(rs.getString(3));
+        return p;
+        */
+        return null;
+    }
+
+    public IUser find(Object idx) {
+
+        Integer id = (Integer) idx;
+
+        IUser p = idMap.get(id);
+        if (p != null) {
+            System.out.println("Get From IDMAP");
+            return p;
+        }
+
+        String req = "SELECT id, username, password FROM user WHERE id=?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(req);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                System.out.println("User not in bd " + id);
+                return null;
+            }
+            // p = this.createUser(rs);
+            idMap.put(id, p);
+            //p.add(UnitOfWork.getInstance());
+            return p;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public IUser findByUsername(String username) {
+        IUser p;
+
+        String req = "SELECT id, username, password FROM user WHERE username=?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(req);
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                System.out.println("not in bd " + username);
+                return null;
+            }
+            p = idMap.get(rs.getInt(1));
+            if (p != null) {
+                System.out.println("Get From IDMAP");
+                return p;
+            }
+            // p = this.createUser(rs);
+            idMap.put(rs.getInt(1), p);
+            //p.add(UnitOfWork.getInstance());
+            return p;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Integer insert(IUser o) {
+        return 0;
+    }
+
+    void delete(IUser o) {
+
+    }
+
+    public void update(IUser o) {
+        System.out.println("Update USER");
+    }
+
+}
