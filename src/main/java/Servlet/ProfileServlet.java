9523c24ce606fd9d2f22dcfc9caf51779d1ffc54
damiensauv <damiensauvalle@gmail.com>
2018-05-07 13:59:38 +0200

    profile

---
+++ src/main/java/Servlet/ProfileServlet.java
@@ -0,0 +1,44 @@
+package Servlet;
+
+import Domain.User;
+import Service.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+public class ProfileServlet extends HttpServlet {
+
+    private UserService userService;
+
+    public ProfileServlet() {
+        this.userService = UserService.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // TODO Check if User Connect
+        HttpSession session = request.getSession();
+        Integer id = (Integer) session.getAttribute("user");
+
+        User user = userService.getUser(id);
+        request.setAttribute("user", user);
+
+        RequestDispatcher view = request.getRequestDispatcher("/Template/Profile.jsp");
+        view.forward(request, response);
+    }
+
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+    }
+}
